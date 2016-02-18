fa3ca24636de8554f7c90404596d6e108fe2d775
fran <franciscogonzalez1307@gmail.com>
2016-02-17 18:35:50 -0700

    Carpeta src

---
+++ src/caja.h
@@ -0,0 +1,12 @@
+/*
+ * Esta cabezera contiene la estructura base de una caja normal que puede
+ * contener un dato.
+ * Autor: Francisco Enrique Córdova González
+ * Última actualización: 17/02/16
+ * Contacto: franciscogonzalez1307@gmail.com
+ */
+#pragma once
+template <class T> struct caja {
+  T var;
+  *caja<T> siguiente;
+};
